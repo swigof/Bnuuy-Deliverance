ad9404c1e9e6d2b0de8ea9128246beddf4bb66dc
SwigOf <33262954+SwigOf@users.noreply.github.com>
2023-04-01 00:12:46 -0400

    wow its moving

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 #include <gb/cgb.h>
 #include "tile_data/tileset_tiles.h"
 #include "tile_data/tileset_map.h"
-#include "helpers/input.h"
+
+#define	I_NORMAL    0x00U
+#define I_MENU      0x01U
+#define I_INVENTORY 0x02U
 
 const uint8_t sprite_data[] = { 
   0x0F,0x0F,0x30,0x30,0x40,0x40,0x40,0x40,
@@ -14,6 +17,10 @@ const uint8_t sprite_data[] = {
   0x21,0x21,0x21,0x21,0x01,0x01,0x01,0x01,
   0x22,0x22,0xC2,0xC2,0x0C,0x0C,0xF0,0xF0
 };
+
+joypads_t joypads;
+uint8_t input_mode = I_NORMAL;
+uint8_t x, y = 75;
 
 int main() {
     NR52_REG = 0x80;
@@ -48,15 +55,44 @@ int main() {
     set_sprite_prop(2, 0);
     set_sprite_prop(3, 0);
 
-    move_sprite(0, 75, 75);
-    move_sprite(1, 75, 75 + 8);
-    move_sprite(2, 75 + 8, 75);
-    move_sprite(3, 75 + 8, 75 + 8);
+    move_sprite(0, x, y);
+    move_sprite(1, x, y + 8);
+    move_sprite(2, x + 8, y);
+    move_sprite(3, x + 8, y + 8);
 
-    input_init();
+    joypad_init(1, &joypads);
 
     while(1) {
-      input_process();
+
+      joypad_ex(&joypads);
+
+      if(joypads.joy0 & J_UP) {
+        y--;
+      } else if(joypads.joy0 & J_DOWN) {
+        y++;
+      }
+
+      if(joypads.joy0 & J_LEFT) {
+        x--;
+      } else if(joypads.joy0 & J_RIGHT) {
+        x++;
+      }
+
+      if(joypads.joy0 & J_A) {
+
+      }
+
+      if(joypads.joy0 & J_B) {
+
+      }
+
+      if(joypads.joy0 & J_SELECT) {
+
+      }
+
+      if(joypads.joy0 & J_START) {
+
+      }
 
       sprite_palettes[1] += 1;
       sprite_palettes[2] += 1;
@@ -65,6 +101,11 @@ int main() {
       if(sprite_palettes[2] > 32767) sprite_palettes[2] = 0;
       if(sprite_palettes[3] > 32767) sprite_palettes[3] = 0;
       set_sprite_palette(0, 1, sprite_palettes);
+
+      move_sprite(0, x, y);
+      move_sprite(1, x, y + 8);
+      move_sprite(2, x + 8, y);
+      move_sprite(3, x + 8, y + 8);
 
       wait_vbl_done();
     }
