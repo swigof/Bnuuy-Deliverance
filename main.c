d8c446c123de6ce8cd57d7ee9042a5b7156582a3
SwigOf <33262954+SwigOf@users.noreply.github.com>
2024-02-12 20:17:31 -0500

    fix spritesheet and add cool ship (heck ya)

--- main.c
+++ main.c
@@ -7,20 +7,11 @@
 #define I_MENU      0x01U
 #define I_INVENTORY 0x02U
 
-const uint8_t sprite_data[] = { 
-  0x0F,0x0F,0x30,0x30,0x40,0x40,0x40,0x40,
-  0x84,0x84,0x84,0x84,0x84,0x84,0x84,0x84,
-  0x84,0x84,0x84,0x84,0x80,0x80,0x80,0x80,
-  0x44,0x44,0x43,0x43,0x30,0x30,0x0F,0x0F,
-  0xF0,0xF0,0x0C,0x0C,0x02,0x02,0x02,0x02,
-  0x21,0x21,0x21,0x21,0x21,0x21,0x21,0x21,
-  0x21,0x21,0x21,0x21,0x01,0x01,0x01,0x01,
-  0x22,0x22,0xC2,0xC2,0x0C,0x0C,0xF0,0xF0
-};
-
 joypads_t joypads;
 uint8_t input_mode = I_NORMAL;
 uint8_t x, y = 75;
+uint8_t flip = 0;
+uint8_t flip_counter = 0;
 
 int main() {
     NR52_REG = 0x80;
@@ -40,25 +31,16 @@ int main() {
     // set_bkg_tiles(0,0,tileset_map_width,tileset_map_height,tileset_map_attr);
     //VBK_REG = VBK_TILES;
 
-    set_sprite_data(0, 4, sprite_data);
+    set_sprite_data(0, tileset_tiles_count, tileset_tiles);
 
-    set_sprite_tile(0, 0);
-    set_sprite_tile(1, 1);
-    set_sprite_tile(2, 2);
-    set_sprite_tile(3, 3);
+    set_sprite_tile(0, 61);
     
-    palette_color_t sprite_palettes[] = { RGB8(0, 0, 0),RGB8(255, 0, 0),RGB8(255, 0, 0),RGB8(255, 0, 0) };
-    set_sprite_palette(0,1,sprite_palettes);
+    palette_color_t sprite_palettes[] = { RGB8(255, 0, 0),RGB8(0, 255, 0),RGB8(0, 0, 255),RGB8(0, 0, 0) };
+    set_sprite_palette(0, 1, sprite_palettes);
 
     set_sprite_prop(0, 0);
-    set_sprite_prop(1, 0);
-    set_sprite_prop(2, 0);
-    set_sprite_prop(3, 0);
 
     move_sprite(0, x, y);
-    move_sprite(1, x, y + 8);
-    move_sprite(2, x + 8, y);
-    move_sprite(3, x + 8, y + 8);
 
     joypad_init(1, &joypads);
 
@@ -79,11 +61,23 @@ int main() {
       }
 
       if(joypads.joy0 & J_A) {
-
+        sprite_palettes[1] += 1;
+        sprite_palettes[2] += 1;
+        sprite_palettes[3] += 1;
+        if(sprite_palettes[1] > 32767) sprite_palettes[1] = 0;
+        if(sprite_palettes[2] > 32767) sprite_palettes[2] = 0;
+        if(sprite_palettes[3] > 32767) sprite_palettes[3] = 0;
+        set_sprite_palette(0, 1, sprite_palettes);
       }
 
       if(joypads.joy0 & J_B) {
-
+        sprite_palettes[1] -= 1;
+        sprite_palettes[2] -= 1;
+        sprite_palettes[3] -= 1;
+        if(sprite_palettes[1] < 0) sprite_palettes[1] = 32767;
+        if(sprite_palettes[2] < 0) sprite_palettes[2] = 32767;
+        if(sprite_palettes[3] < 0) sprite_palettes[3] = 32767;
+        set_sprite_palette(0, 1, sprite_palettes);
       }
 
       if(joypads.joy0 & J_SELECT) {
@@ -94,18 +88,20 @@ int main() {
 
       }
 
-      sprite_palettes[1] += 1;
-      sprite_palettes[2] += 1;
-      sprite_palettes[3] += 1;
-      if(sprite_palettes[1] > 32767) sprite_palettes[1] = 0;
-      if(sprite_palettes[2] > 32767) sprite_palettes[2] = 0;
-      if(sprite_palettes[3] > 32767) sprite_palettes[3] = 0;
-      set_sprite_palette(0, 1, sprite_palettes);
+      flip_counter++;
+      if(flip_counter == 3) {
+        flip = !flip;
+        flip_counter = 0;
+        if(flip)
+          set_sprite_prop(0, S_FLIPX);
+        else
+          set_sprite_prop(0, 0);
+      }
 
       move_sprite(0, x, y);
-      move_sprite(1, x, y + 8);
-      move_sprite(2, x + 8, y);
-      move_sprite(3, x + 8, y + 8);
+      // scroll_sprite() ?
+
+      // fix blurring on movement?
 
       wait_vbl_done();
     }
