afa26ee496f24679c08df20c00a17dd2c5a91584
SwigOf <33262954+SwigOf@users.noreply.github.com>
2024-05-12 13:54:58 -0400

    migrate to platformer, add subpixel movement

--- helpers/entity.h
+++ helpers/entity.h
@@ -1,10 +1,44 @@
 #include <stdint.h>
 #include <gb/gb.h>
 
+#define	SUBPIXEL_THRESHOLD 0x02U
+
 typedef struct {
-    uint8_t x, y, prop;
+    uint8_t x, y, prop, sub_x, sub_y, direction;
     uint8_t flags; // render, tile/sprite, ? ...
 } entity_t;
+
+// TODO 
+// used renamed joypad direction constants?
+// add subpixel thresholds constants for different entities
+
+inline void move_entity_left(entity_t* entity) {
+    if(entity->direction & J_LEFT) {
+        entity->sub_x++;
+    } else {
+        entity->direction &= ~J_RIGHT;
+        entity->direction |= J_LEFT;
+        entity->sub_x = 1;
+    }
+    if(entity->sub_x >= 2) {
+        entity->sub_x = 0;
+        entity->x--;
+    }
+}
+
+inline void move_entity_right(entity_t* entity) {
+    if(entity->direction & J_RIGHT) {
+        entity->sub_x++;
+    } else {
+        entity->direction &= ~J_LEFT;
+        entity->direction |= J_RIGHT;
+        entity->sub_x = 1;
+    }
+    if(entity->sub_x >= 2) {
+        entity->sub_x = 0;
+        entity->x++;
+    }
+}
 
 inline void update_entity(entity_t entity) {
     set_sprite_prop(0, entity.prop);
--- main.c
+++ main.c
@@ -4,17 +4,10 @@
 #include "tile_data/tileset_tiles.h"
 #include "tile_data/tileset_map.h"
 
-#define	I_NORMAL    0x00U
-#define I_MENU      0x01U
-#define I_INVENTORY 0x02U
-
 joypads_t joypads;
-uint8_t input_mode = I_NORMAL;
 
-entity_t ship;
-uint8_t flip_counter = 0;
+entity_t player;
 uint8_t firing_charge = 0;
-uint8_t next_shot_speed = 2;
 
 int main() {
     NR52_REG = 0x80;
@@ -36,84 +29,51 @@ int main() {
 
     set_sprite_data(0, tileset_tiles_count, tileset_tiles);
 
-    set_sprite_tile(0, 61);
+    set_sprite_tile(0, 58);
     
     palette_color_t sprite_palettes[] = { RGB8(255, 0, 0),RGB8(0, 255, 0),RGB8(0, 0, 255),RGB8(0, 0, 0) };
     set_sprite_palette(0, 1, sprite_palettes);
 
     set_sprite_prop(0, 0);
 
-    move_sprite(0, ship.x, ship.y);
+    player.x = 100;
+    player.y = 100;
+    move_sprite(0, player.x, player.y);
 
     joypad_init(1, &joypads);
 
     while(1) {
+        joypad_ex(&joypads);
 
-      joypad_ex(&joypads);
-
-      if(joypads.joy0 & J_UP) {
-        ship.y--;
-        next_shot_speed = 3;
-      } else if(joypads.joy0 & J_DOWN) {
-        ship.y++;
-        next_shot_speed = 1;
-      }
-
-      if(joypads.joy0 & J_LEFT) {
-        ship.x--;
-      } else if(joypads.joy0 & J_RIGHT) {
-        ship.x++;
-      }
-
-      if(joypads.joy0 & J_A) {
-        // Render sprite / palette change and sound for charging
-        firing_charge++;
-      } else if (firing_charge != 0) {
-        if(firing_charge < 15) {
-          // Failure
+        if(joypads.joy0 & J_UP) {
+            
         }
-        if(firing_charge == 15) {
-          // Special shot (with aoe)?
-        } else  {
-          // Normal shot
+
+        if(joypads.joy0 & J_LEFT) {
+            move_entity_left(&player);
+        } else if(joypads.joy0 & J_RIGHT) {
+            move_entity_right(&player);
         }
-        next_shot_speed = 2;
-        firing_charge = 0;
-      }
 
-      if(joypads.joy0 & J_B) {
-        // lock to change face / movement?
+        if(joypads.joy0 & J_A) {
+            // jump
+        }
 
-        // sprite_palettes[1] -= 1;
-        // sprite_palettes[2] -= 1;
-        // sprite_palettes[3] -= 1;
-        // if(sprite_palettes[1] < 0) sprite_palettes[1] = 32767;
-        // if(sprite_palettes[2] < 0) sprite_palettes[2] = 32767;
-        // if(sprite_palettes[3] < 0) sprite_palettes[3] = 32767;
-        // set_sprite_palette(0, 1, sprite_palettes);
-      }
+        if(joypads.joy0 & J_B) {
+            // interact / attack
+        }
 
-      if(joypads.joy0 & J_SELECT) {
+        if(joypads.joy0 & J_SELECT) {
 
-      }
+        }
 
-      if(joypads.joy0 & J_START) {
+        if(joypads.joy0 & J_START) {
 
-      }
+        }
 
-      flip_counter++;
-      if(flip_counter == 3) {
-        ship.prop ^= S_FLIPX;
-        flip_counter = 0;
-      }
+        update_entity(player);
 
-      update_entity(ship);
-
-      // scroll_sprite() ?
-
-      // fix blurring on movement?
-
-      wait_vbl_done();
+        wait_vbl_done();
     }
 
     return 0;
