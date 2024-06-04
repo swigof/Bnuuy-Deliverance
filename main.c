f85489fe4f69dd49d3cee092dd43dc6ac10e9771
SwigOf <33262954+SwigOf@users.noreply.github.com>
2024-06-03 22:38:37 -0400

    fix initial bkg rendering

--- main.c
+++ main.c
@@ -4,22 +4,27 @@
 #include "tile_data/tileset_tiles.h"
 #include "tile_data/tileset_map.h"
 
+#define MAX_Y ((tileset_map_height - 18) * 8)
+#define MIN_Y 0
+#define MAX_X 20 * 8
+#define MIN_X 0
+
 joypads_t joypads;
 
 entity_t player;
 uint8_t map_pos_y, old_map_pos_y, redraw;
 
 void set_camera() {
-    SCY_REG = player.y; 
+    SCY_REG = player.y;
     map_pos_y = (uint8_t)(player.y >> 3u);
-    if (map_pos_y != old_map_pos_y) { 
+    if (map_pos_y != old_map_pos_y) {
         if (player.direction & J_UP) {
             set_bkg_submap(0, map_pos_y, 20, 1, tileset_map, 20);
         } else {
-            if ((tileset_map_height - 18u) > map_pos_y) 
-                set_bkg_submap(0, map_pos_y + 18u, 20, 1, tileset_map, 20);     
+            if ((tileset_map_height - 18u) > map_pos_y)
+                set_bkg_submap(0, map_pos_y + 18u, 20, 1, tileset_map, 20);
         }
-        old_map_pos_y = map_pos_y; 
+        old_map_pos_y = map_pos_y;
     }
 }
 
@@ -28,30 +33,35 @@ int main() {
     NR50_REG = 0x77;
     NR51_REG = 0xFF;
 
-    DISPLAY_ON;
+    DISPLAY_OFF;
     SHOW_BKG;
     SPRITES_8x8;
     SHOW_SPRITES;
-
     VBK_REG = VBK_TILES;
-    set_bkg_palette(0,1,tileset_map_colors);
     set_bkg_data(0,tileset_tiles_count,tileset_tiles);
-    set_bkg_submap(0, 0, 20, 18, tileset_map, 20);
+    set_bkg_palette(0,1,tileset_map_colors);
+
+    old_map_pos_y = 255;
+    map_pos_y = 0;
+    set_bkg_submap(0, map_pos_y, 20, 18, tileset_map, 20);
+    DISPLAY_ON;
+
+    player.x = 0;
+    player.y = 0;
+
+    redraw = FALSE;
+
+    SCY_REG = player.y;
+
     // VBK_REG = VBK_ATTRIBUTES;
     // set_bkg_tiles(0,0,tileset_map_width,tileset_map_height,tileset_map_attr);
     // VBK_REG = VBK_TILES;
 
     set_sprite_data(0, tileset_tiles_count, tileset_tiles);
-
     set_sprite_tile(0, 58);
-    
     palette_color_t sprite_palettes[] = { RGB8(255, 0, 0),RGB8(0, 255, 0),RGB8(0, 0, 255),RGB8(0, 0, 0) };
     set_sprite_palette(0, 1, sprite_palettes);
-
     set_sprite_prop(0, 0);
-
-    player.x = 100;
-    player.y = 100;
     move_sprite(0, player.x, player.y);
 
     joypad_init(1, &joypads);
@@ -61,17 +71,23 @@ int main() {
 
         // just for testing
         if(joypads.joy0 & J_UP) {
-            move_entity_up(&player);
-            set_camera();
+            if (player.y > MIN_Y) {
+                move_entity_up(&player);
+                redraw = TRUE;
+            }
         } else if(joypads.joy0 & J_DOWN) {
-            move_entity_down(&player);
-            set_camera();
+            if (player.y < MAX_Y) {
+                move_entity_down(&player);
+                redraw = TRUE;
+            }
         }
 
         if(joypads.joy0 & J_LEFT) {
-            move_entity_left(&player);
+            if (player.x > MIN_X)
+                move_entity_left(&player);
         } else if(joypads.joy0 & J_RIGHT) {
-            move_entity_right(&player);
+            if (player.x < MAX_X)
+                move_entity_right(&player);
         }
 
         if(joypads.joy0 & J_A) {
@@ -92,7 +108,11 @@ int main() {
 
         update_entity(player);
 
-        wait_vbl_done();
+        if (redraw) {
+            wait_vbl_done();
+            set_camera();
+            redraw = FALSE;
+        } else wait_vbl_done();
     }
 
     return 0;
