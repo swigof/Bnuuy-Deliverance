71d62e0133a498efb794ef93482ae1777164e9b8
SwigOf <33262954+SwigOf@users.noreply.github.com>
2024-06-04 22:53:11 -0400

    fix the camera (wowzers)

--- main.c
+++ main.c
@@ -4,19 +4,21 @@
 #include "tile_data/tileset_tiles.h"
 #include "tile_data/tileset_map.h"
 
-#define MAX_Y ((tileset_map_height - 18) * 8)
-#define MIN_Y 0
-#define MAX_X 20 * 8
-#define MIN_X 0
+#define MAX_CAMERA_Y ((tileset_map_height - 18) * 8)
+#define MAX_PLAYER_Y 152
+#define MIN_PLAYER_Y 16
+#define MAX_PLAYER_X 160
+#define MIN_PLAYER_X 8
 
 joypads_t joypads;
 
 entity_t player;
 uint8_t map_pos_y, old_map_pos_y, redraw;
+uint16_t camera_y;
 
 void set_camera() {
-    SCY_REG = player.y;
-    map_pos_y = (uint8_t)(player.y >> 3u);
+    SCY_REG = camera_y;
+    map_pos_y = (uint8_t)(camera_y >> 3u);
     if (map_pos_y != old_map_pos_y) {
         if (player.direction & J_UP) {
             set_bkg_submap(0, map_pos_y, 20, 1, tileset_map, 20);
@@ -46,8 +48,9 @@ int main() {
     set_bkg_submap(0, map_pos_y, 20, 18, tileset_map, 20);
     DISPLAY_ON;
 
-    player.x = 0;
-    player.y = 0;
+    player.x = MIN_PLAYER_X;
+    player.y = 0; // TODO place player at bottom, make map longer to see if max camera y breaks?
+    camera_y = 0;
 
     redraw = FALSE;
 
@@ -71,22 +74,30 @@ int main() {
 
         // just for testing
         if(joypads.joy0 & J_UP) {
-            if (player.y > MIN_Y) {
+            if(player.y > MIN_PLAYER_Y) {
                 move_entity_up(&player);
-                redraw = TRUE;
+                if (player.sub_y == 0 && player.y < 72 && camera_y > 0) {
+                    player.y = 72;
+                    camera_y--;
+                    redraw = TRUE;
+                }
             }
         } else if(joypads.joy0 & J_DOWN) {
-            if (player.y < MAX_Y) {
+            if(player.y < MAX_PLAYER_Y) {
                 move_entity_down(&player);
-                redraw = TRUE;
+                if (player.sub_y == 0 && player.y > 72 && camera_y < MAX_CAMERA_Y) {
+                    player.y = 72;
+                    camera_y++;
+                    redraw = TRUE;
+                }
             }
         }
 
         if(joypads.joy0 & J_LEFT) {
-            if (player.x > MIN_X)
+            if (player.x > MIN_PLAYER_X)
                 move_entity_left(&player);
         } else if(joypads.joy0 & J_RIGHT) {
-            if (player.x < MAX_X)
+            if (player.x < MAX_PLAYER_X)
                 move_entity_right(&player);
         }
 
