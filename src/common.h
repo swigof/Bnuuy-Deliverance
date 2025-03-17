60d34489a5fea7e63702eefcceddff27184afb62
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-03-17 06:50:09 -0400

    fix bit selection helpers

--- src/common.h
+++ src/common.h
@@ -1,8 +1,8 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#define HIGH_BITS(v) v >> 4
-#define LOW_BITS(v) v & 0b00001111
+#define HIGH_BITS(v) (v >> 4)
+#define LOW_BITS(v) (v & 0b00001111)
 
 #define MIN(A,B) ((A)<(B)?(A):(B))
 #define MAX(A,B) ((A)>(B)?(A):(B))
