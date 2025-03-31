c8619ed85807f839b29d71c30ca03e7c412770ab
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-03-31 13:49:42 -0400

    make default end having a sit

--- src/level_end.c
+++ src/level_end.c
@@ -63,125 +63,159 @@ void level_end_update(void) {
         vsync();
     }
 
-    // prevent player from carrying box, move box into position
-    carry = FALSE;
-    box->update_function = (void (*)(void *)) &move_box_to_position;
-    end_cutscene_timer = 0;
-    while(end_cutscene_timer < 120) {
+    if(!true_end_flag) {
+        // be grounded
+        joypads.joy0 = 0;
         prev_joypads = joypads;
-        joypad_ex(&joypads);
-        joypads.joy0 &= ~J_B;
+        while(!(GROUNDED & player->state)) {
+            update_entities();
+            vsync();
+        }
         update_entities();
         vsync();
-        end_cutscene_timer++;
-    }
+        while(!(GROUNDED & box->state)) {
+            update_entities();
+            vsync();
+        }
 
-    // shake box
-    initrand(0); // use static seed to preserve same behavior
-    while(box_shake_counter < BOX_SHAKE_COUNT) {
-        end_cutscene_timer = rand();
-        if(end_cutscene_timer < 30)
-            end_cutscene_timer = 30;
-        else if(end_cutscene_timer > 120)
-            end_cutscene_timer = 120;
-        box->x += (int8_t)rand();
-        box->y += (int8_t)rand();
-        while(end_cutscene_timer != 0) {
+        // sigh
+        player->update_function = NULL;
+        player->animation_frame = 0;
+        player->frame_counter = 0;
+        player->state_data = &player_sigh;
+        while(player->frame_counter != 100 || player->animation_frame != 2) {
+            update_entities();
+            vsync();
+        }
+
+        //pan up
+        while(camera_y > 8) {
+            update_entities();
+            camera_y--;
+            update_camera();
+            vsync();
+        }
+        player->active = FALSE;
+        box->active = FALSE;
+    } else {
+        // prevent player from carrying box, move box into position
+        carry = FALSE;
+        box->update_function = (void (*)(void *)) &move_box_to_position;
+        end_cutscene_timer = 0;
+        while (end_cutscene_timer < 120) {
             prev_joypads = joypads;
             joypad_ex(&joypads);
             joypads.joy0 &= ~J_B;
             update_entities();
             vsync();
-            end_cutscene_timer--;
+            end_cutscene_timer++;
         }
-        box_shake_counter++;
-    }
 
-    // drop box and despawn it
-    while(MAP_COORD(box->y) < 305) {
-        prev_joypads = joypads;
-        joypad_ex(&joypads);
-        joypads.joy0 &= ~J_B;
-        update_entities();
-        vsync();
-    }
-    box->active = FALSE;
-    end_cutscene_timer = 0;
-    while(end_cutscene_timer < 120) {
-        prev_joypads = joypads;
-        joypad_ex(&joypads);
-        update_entities();
-        vsync();
-        end_cutscene_timer++;
-    }
+        // shake box
+        initrand(0); // use static seed to preserve same behavior
+        while (box_shake_counter < BOX_SHAKE_COUNT) {
+            end_cutscene_timer = rand();
+            if (end_cutscene_timer < 30)
+                end_cutscene_timer = 30;
+            else if (end_cutscene_timer > 120)
+                end_cutscene_timer = 120;
+            box->x += (int8_t) rand();
+            box->y += (int8_t) rand();
+            while (end_cutscene_timer != 0) {
+                prev_joypads = joypads;
+                joypad_ex(&joypads);
+                joypads.joy0 &= ~J_B;
+                update_entities();
+                vsync();
+                end_cutscene_timer--;
+            }
+            box_shake_counter++;
+        }
 
-    // spawn angel, fly it up
-    entity_to_add.base_tile = player_sheet_TILE_COUNT + box_sheet_TILE_COUNT + truck_sheet_TILE_COUNT;
-    entity_to_add.prop = 3;
-    entity_to_add.active = TRUE;
-    entity_to_add.state_data = &angel_tpose;
-    entity_to_add.x = 80 << 4;
-    entity_to_add.y = 336 << 4;
-    entity_to_add.update_function = (void (*)(void *)) &update_angel;
-    angel = add_entity();
-    while(MAP_COORD(angel->y) > 212) {
-        prev_joypads = joypads;
-        joypad_ex(&joypads);
-        if(MAP_COORD(angel->y) > 273)
-            angel->vel_y--;
-        else
-            angel->vel_y++;
-        angel->y += angel->vel_y;
-        update_entities();
-        vsync();
-    }
-    while(MAP_COORD(angel->y) < 216) {
-        prev_joypads = joypads;
-        joypad_ex(&joypads);
-        angel->vel_y++;
-        angel->y += angel->vel_y;
-        update_entities();
-        vsync();
-    }
-
-    // idle
-    angel->state_data = &angel_idle;
-    end_cutscene_timer = 0;
-    while(end_cutscene_timer < 120) {
-        prev_joypads = joypads;
-        joypad_ex(&joypads);
-        update_entities();
-        vsync();
-        end_cutscene_timer++;
-    }
-
-    //clap for the true gamer
-    if(true_end_flag) {
-        angel->state_data = &angel_clap;
+        // drop box and despawn it
+        while (MAP_COORD(box->y) < 305) {
+            prev_joypads = joypads;
+            joypad_ex(&joypads);
+            joypads.joy0 &= ~J_B;
+            update_entities();
+            vsync();
+        }
+        box->active = FALSE;
         end_cutscene_timer = 0;
-        while(end_cutscene_timer < 180) {
+        while (end_cutscene_timer < 120) {
             prev_joypads = joypads;
             joypad_ex(&joypads);
             update_entities();
             vsync();
             end_cutscene_timer++;
         }
-    }
 
-    //fly away and pan up
-    angel->state_data = &angel_tpose;
-    while(camera_y > 8) {
-        prev_joypads = joypads;
-        joypad_ex(&joypads);
-        angel->vel_y--;
-        angel->y += angel->vel_y;
-        update_entities();
-        camera_y--;
-        update_camera();
-        vsync();
+        // spawn angel, fly it up
+        entity_to_add.base_tile = player_sheet_TILE_COUNT + box_sheet_TILE_COUNT + truck_sheet_TILE_COUNT;
+        entity_to_add.prop = 3;
+        entity_to_add.active = TRUE;
+        entity_to_add.state_data = &angel_tpose;
+        entity_to_add.x = 80 << 4;
+        entity_to_add.y = 336 << 4;
+        entity_to_add.update_function = (void (*)(void *)) &update_angel;
+        angel = add_entity();
+        while (MAP_COORD(angel->y) > 212) {
+            prev_joypads = joypads;
+            joypad_ex(&joypads);
+            if (MAP_COORD(angel->y) > 273)
+                angel->vel_y--;
+            else
+                angel->vel_y++;
+            angel->y += angel->vel_y;
+            update_entities();
+            vsync();
+        }
+        while (MAP_COORD(angel->y) < 216) {
+            prev_joypads = joypads;
+            joypad_ex(&joypads);
+            angel->vel_y++;
+            angel->y += angel->vel_y;
+            update_entities();
+            vsync();
+        }
+
+        // idle
+        angel->state_data = &angel_idle;
+        end_cutscene_timer = 0;
+        while (end_cutscene_timer < 120) {
+            prev_joypads = joypads;
+            joypad_ex(&joypads);
+            update_entities();
+            vsync();
+            end_cutscene_timer++;
+        }
+
+        //clap
+        angel->state_data = &angel_clap;
+        end_cutscene_timer = 0;
+        while (end_cutscene_timer < 180) {
+            prev_joypads = joypads;
+            joypad_ex(&joypads);
+            update_entities();
+            vsync();
+            end_cutscene_timer++;
+        }
+
+        //fly away and pan up
+        angel->state_data = &angel_tpose;
+        while(camera_y > 8) {
+            prev_joypads = joypads;
+            joypad_ex(&joypads);
+            angel->vel_y--;
+            angel->y += angel->vel_y;
+            update_entities();
+            camera_y--;
+            update_camera();
+            vsync();
+        }
+        angel->active = FALSE;
+        player->active = FALSE;
     }
-    angel->active = FALSE;
-    player->active = FALSE;
 }
 
 const level_t level_end = {
